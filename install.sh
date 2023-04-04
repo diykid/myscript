e14d9f4f14b8020a46dfaa1d34dce0876516ad65
Aaron <532054+diykid@users.noreply.github.com>
2023-04-04 18:45:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ if [ ! -d ~/.shardeum ]; then
 fi
 sudo  wget -N -q -O ./installdocker.sh https://raw.githubusercontent.com/diykid/myscript/main/installdocker.sh && chmod +x ./installdocker.sh
 sudo  wget -N -q -O ./installvalidator.sh  https://raw.githubusercontent.com/diykid/myscript/main/installvalidator.sh && chmod +x ./installvalidator.sh 
-sudo  wget -N -q -O .shardeum/watchdog.sh https://raw.githubusercontent.com/bombermine3/nodes/main/shardeum/watchdog.sh && chmod +x .shardeum/watchdog.sh
 
 sudo ./installdocker.sh
-#./installvalidator.sh
+./installvalidator.sh
 #sudo -u ubuntu sh -c 'echo "*/5 * * * * ubuntu bash /home/ubuntu/.shardeum/watchdog.sh >> /home/ubuntu/.shardeum/watchdog.log" >> /etc/crontab'
+
+#sudo  wget -N -q -O .shardeum/watchdog.sh https://raw.githubusercontent.com/bombermine3/nodes/main/shardeum/watchdog.sh && chmod +x .shardeum/watchdog.sh
