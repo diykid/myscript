0f2c3e1d2646e446b4fc15ae517929b5e9dae83b
Aaron <532054+diykid@users.noreply.github.com>
2023-04-04 18:21:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 
 cd /home/ubuntu
+mkdir .shardeum
 sudo wget -q -O ./installdocker.sh https://raw.githubusercontent.com/diykid/myscript/main/installdocker.sh & chmod +x ./installdocker.sh
 sudo wget -q -O ./installvalidator.sh  https://raw.githubusercontent.com/diykid/myscript/main/installvalidator.sh & chmod +x ./installvalidator.sh 
 sudo wget -q -O .shardeum/watchdog.sh https://raw.githubusercontent.com/bombermine3/nodes/main/shardeum/watchdog.sh & chmod +x .shardeum/watchdog.sh
