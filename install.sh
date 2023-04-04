df867e1df999fd5a2e546ee1034941fec75c226b
Aaron <532054+diykid@users.noreply.github.com>
2023-04-04 18:18:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 
-echo install docker 
-sudo wget -q -O /home/ubuntu/installdocker.sh https://raw.githubusercontent.com/diykid/myscript/main/installdocker.sh & chmod +x /home/ubuntu/installdocker.sh
-sudo wget -q -O /home/ubuntu/installvalidator.sh  https://raw.githubusercontent.com/diykid/myscript/main/installvalidator.sh & chmod +x /home/ununtu/installvalidator.sh 
-sudo wget -q -O /home/ubuntu/.shardeum/watchdog.sh https://raw.githubusercontent.com/bombermine3/nodes/main/shardeum/watchdog.sh & chmod +x /home/ubuntu/.shardeum/watchdog.sh
+cd /home/ubuntu
+sudo wget -q -O ./installdocker.sh https://raw.githubusercontent.com/diykid/myscript/main/installdocker.sh & chmod +x ./installdocker.sh
+sudo wget -q -O ./installvalidator.sh  https://raw.githubusercontent.com/diykid/myscript/main/installvalidator.sh & chmod +x ./installvalidator.sh 
+sudo wget -q -O .shardeum/watchdog.sh https://raw.githubusercontent.com/bombermine3/nodes/main/shardeum/watchdog.sh & chmod +x .shardeum/watchdog.sh
 
-sudo /home/ubuntu/installdocker.sh
-sudo /home/ubuntu/installvalidator.sh
+sudo ./installdocker.sh
+sudo ./installvalidator.sh
 sudo sh -c 'echo "*/5 * * * * ubuntu bash /home/ubuntu/.shardeum/watchdog.sh >> /home/ubuntu/.shardeum/watchdog.log" >> /etc/crontab'
