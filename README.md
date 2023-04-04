d02002dc2082a3111fa3786952a17bfb4e0b46e6
Aaron <532054+diykid@users.noreply.github.com>
2023-04-04 20:15:02 +0800

    abc

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # myscript
 自己收集的一些常用脚本
 curl -O https://raw.githubusercontent.com/diykid/myscript/main/install.sh && chmod +x install.sh && ./install.sh
+
+sudo wget -N -q -O ./install.sh https://raw.githubusercontent.com/diykid/myscript/main/install.sh && sudo chmod +x ./install.sh &&  ./install.sh
--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 cd /home/ubuntu
-sudo -u ubuntu wget -N -q -O ./installdocker.sh https://raw.githubusercontent.com/diykid/myscript/main/installdocker.sh && sudo chmod +x ./installdocker.sh && ./installdocker.sh
+sudo wget -N -q -O ./installdocker.sh https://raw.githubusercontent.com/diykid/myscript/main/installdocker.sh && sudo chmod +x ./installdocker.sh && ./installdocker.sh
 
-sudo -u ubuntu wget -N -q -O ./installvalidator.sh  https://raw.githubusercontent.com/diykid/myscript/main/installvalidator.sh && sudo chmod +x ./installvalidator.sh && ./installvalidator.sh
+sudo wget -N -q -O ./installvalidator.sh  https://raw.githubusercontent.com/diykid/myscript/main/installvalidator.sh && sudo chmod +x ./installvalidator.sh && sudo ./installvalidator.sh
 
 
 #sudo  wget -N -q -O .shardeum/watchdog.sh https://raw.githubusercontent.com/bombermine3/nodes/main/shardeum/watchdog.sh && chmod +x .shardeum/watchdog.sh
 #sudo sh -c 'echo "*/5 * * * * ubuntu bash /home/ubuntu/.shardeum/watchdog.sh >> /home/ubuntu/.shardeum/watchdog.log" >> /etc/crontab'
 
-sudo -u ubuntu wget -q -O /home/ubuntu/.shardeum/watchdog.sh https://raw.githubusercontent.com/bombermine3/nodes/main/shardeum/watchdog.sh && sudo chmod +x /home/ubuntu/.shardeum/watchdog.sh && sudo sh -c 'echo "*/5 * * * * ubuntu bash /home/ubuntu/.shardeum/watchdog.sh >> /home/ubuntu/.shardeum/watchdog.log" >> /etc/crontab'
+sudo wget -N -q -O /home/ubuntu/.shardeum/watchdog.sh https://raw.githubusercontent.com/bombermine3/nodes/main/shardeum/watchdog.sh && sudo chmod +x /home/ubuntu/.shardeum/watchdog.sh
+sudo sh -c 'echo "*/5 * * * * root bash /home/ubuntu/.shardeum/watchdog.sh >> /home/ubuntu/.shardeum/watchdog.log" >> /etc/crontab'
+sudo sh -c 'echo "*/5 * * * * ubuntu bash /home/ubuntu/.shardeum/watchdog.sh >> /home/ubuntu/.shardeum/watchdog.log" >> /etc/crontab'
